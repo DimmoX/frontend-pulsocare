295287fde291e25778158f688bd64a946caf3534
danis <dsanjuan1999@gmail.com>
2026-07-12 20:08:56 -0400

    corrección de error en redirectUri

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,7 +25,7 @@ export const appConfig: ApplicationConfig = {
             clientId: 'bbc1023b-e89e-4fd1-925c-141f8d7d148c',
             authority: 'https://pulsocareduoc.b2clogin.com/pulsocareduoc.onmicrosoft.com/B2C_1_SIGN_IN',
             knownAuthorities: ['pulsocareduoc.b2clogin.com'],
-            redirectUri: 'window.location.origin'
+            redirectUri: 'https://main.d3kr9b0z736ylh.amplifyapp.com/'
           },
           cache: {
             cacheLocation: 'localStorage'
